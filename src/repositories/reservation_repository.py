9f3dc4196aa85274d6d0bcc700f4cf934545e20a
Caua Carvalho <cauaccarvalho10@gmail.com>
2026-04-10 23:39:20 -0300

    feat: add reservation repository

---
+++ src/repositories/reservation_repository.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Session
+from src.models import Reservation
+from uuid import UUID
+
+
+class ReservationRepository:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def create(self, reservation_data: dict) -> Reservation:
+        db_reservation = Reservation(**reservation_data)
+        self.db.add(db_reservation)
+        self.db.commit()
+        self.db.refresh(db_reservation)
+        return db_reservation
