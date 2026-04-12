bae4ce9bbeb20ccf040cc9d282fd1119220fdadb
Caua Carvalho <cauaccarvalho10@gmail.com>
2026-04-12 15:49:18 -0300

    feat: add database dependency provider

--- src/db/session.py
+++ src/db/session.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from src.core.config import DATABASE_URL
 
+
 engine = create_engine(
     DATABASE_URL,
     connect_args={"sslmode": "require"},
@@ -13,3 +14,11 @@ SessionLocal = sessionmaker(
     bind=engine,
     autoflush=False,
 )
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
