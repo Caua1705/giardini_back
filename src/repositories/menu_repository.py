8e41fb3fec6e202612b177f7b0ad7dfd42220f93
Caua Carvalho <cauaccarvalho10@gmail.com>
2026-05-14 00:32:33 -0300

    feat: implement MenuRepository to fetch menu data from database view

--- src/repositories/menu_repository.py
+++ src/repositories/menu_repository.py
@@ -7,6 +7,6 @@ class MenuRepository:
         self.db = db
 
     def get_menu(self) -> list[dict]:
-        query = text("SELECT * FROM public.menu_public_view")
+        query = text("SELECT * FROM giardini_site.menu_public_view")
         result = self.db.execute(query).mappings().all()
         return [dict(row) for row in result]
