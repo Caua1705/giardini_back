c198117fd38fd1bbb0d1fd9e0dcb5f066c11a0d4
Caua Carvalho <cauaccarvalho10@gmail.com>
2026-05-15 23:46:15 -0300

    feat: restrict CORS allow_origins to specific authorized domains

--- main.py
+++ main.py
@@ -9,9 +9,18 @@ app = FastAPI(
     version="0.1.0",
 )
 
+origins = [
+    "https://giardini.cafe",
+    "https://www.giardini.cafe",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
